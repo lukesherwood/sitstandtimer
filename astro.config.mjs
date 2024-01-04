bc43a44f2dde6143803f5a54871509c10ad8a6a2
Luke Sherwood <luke.sherwood@covermymeds.com>
2024-01-04 10:37:06 -0500

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
+  // Enable Svelte to support Svelte components.
+  integrations: [svelte(), tailwind()],
 });
