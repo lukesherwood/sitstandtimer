f32b7cd5a8cb9c9f1aefb4c96acd1fa59a1798e6
Luke Sherwood <luke.sherwood55@gmail.com>
2025-06-18 07:08:27 -0400

    add netlify and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+
+  adapter: netlify()
 })
